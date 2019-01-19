16c718b4c26df67d458768b53e1896aae28da046
zgy <718138559@qq.com>
2019-01-19 23:27:59 +0800

    增加客户端copy脚本 移除test ignore客户端生成

---
+++ copyProto.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+targetDir = '.clientProto'
+
+def copyProto():
+        fileList = os.listdir()
+        isExists = os.path.exists(targetDir)
+        if isExists :
+                os.rmdir(targetDir)
+        os.makedirs(targetDir)
+        for fileName in fileList :
+                if fileName.find('.') < 0 and fileName != targetDir:
+                        for protoName in os.listdir(fileName) :
+                                if protoName.find('.proto') >= 0:
+                                        shutil.copy(fileName + '/' + protoName, targetDir)
+        
+copyProto()
+# protoc -I shop --go_out=shop --micro_out=shop shop/shop.proto
+# pbjs -t static-module -w commonjs -o ./assets/script/typings/proto.js ./proto/*.proto
+# pbjs -t static-module -w commonjs -o ./assets/script/typings/proto.js ./proto/*.proto --no-comments --no-beautify --no-verify --no-convert --no-delimited
