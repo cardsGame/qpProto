b408ecb291cc7de9f1aa54b267604591c9ee30ed
PengJu.Duan <dpj@tcfellow.com>
2019-01-28 19:41:21 +0800

    HuType

--- notice/notice.pb.go
+++ notice/notice.pb.go
@@ -3,12 +3,13 @@
 
 package notice
 
+import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
+import math "math"
+
 import (
-	context "context"
-	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
+	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -20,7 +21,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type NoticeType int32
 
@@ -33,7 +34,6 @@ var NoticeType_name = map[int32]string{
 	0: "lvse",
 	1: "daili",
 }
-
 var NoticeType_value = map[string]int32{
 	"lvse":  0,
 	"daili": 1,
@@ -42,9 +42,8 @@ var NoticeType_value = map[string]int32{
 func (x NoticeType) String() string {
 	return proto.EnumName(NoticeType_name, int32(x))
 }
-
 func (NoticeType) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_a4805187b6be1089, []int{0}
+	return fileDescriptor_notice_49e39b4df5242b86, []int{0}
 }
 
 type NoticeData struct {
@@ -59,17 +58,16 @@ func (m *NoticeData) Reset()         { *m = NoticeData{} }
 func (m *NoticeData) String() string { return proto.CompactTextString(m) }
 func (*NoticeData) ProtoMessage()    {}
 func (*NoticeData) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4805187b6be1089, []int{0}
+	return fileDescriptor_notice_49e39b4df5242b86, []int{0}
 }
-
 func (m *NoticeData) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_NoticeData.Unmarshal(m, b)
 }
 func (m *NoticeData) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_NoticeData.Marshal(b, m, deterministic)
 }
-func (m *NoticeData) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_NoticeData.Merge(m, src)
+func (dst *NoticeData) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_NoticeData.Merge(dst, src)
 }
 func (m *NoticeData) XXX_Size() int {
 	return xxx_messageInfo_NoticeData.Size(m)
@@ -106,17 +104,16 @@ func (m *CreateNoticeRequest) Reset()         { *m = CreateNoticeRequest{} }
 func (m *CreateNoticeRequest) String() string { return proto.CompactTextString(m) }
 func (*CreateNoticeRequest) ProtoMessage()    {}
 func (*CreateNoticeRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4805187b6be1089, []int{1}
+	return fileDescriptor_notice_49e39b4df5242b86, []int{1}
 }
-
 func (m *CreateNoticeRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_CreateNoticeRequest.Unmarshal(m, b)
 }
 func (m *CreateNoticeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_CreateNoticeRequest.Marshal(b, m, deterministic)
 }
-func (m *CreateNoticeRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CreateNoticeRequest.Merge(m, src)
+func (dst *CreateNoticeRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateNoticeRequest.Merge(dst, src)
 }
 func (m *CreateNoticeRequest) XXX_Size() int {
 	return xxx_messageInfo_CreateNoticeRequest.Size(m)
@@ -152,17 +149,16 @@ func (m *DeleteNoticeRequest) Reset()         { *m = DeleteNoticeRequest{} }
 func (m *DeleteNoticeRequest) String() string { return proto.CompactTextString(m) }
 func (*DeleteNoticeRequest) ProtoMessage()    {}
 func (*DeleteNoticeRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4805187b6be1089, []int{2}
+	return fileDescriptor_notice_49e39b4df5242b86, []int{2}
 }
-
 func (m *DeleteNoticeRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_DeleteNoticeRequest.Unmarshal(m, b)
 }
 func (m *DeleteNoticeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_DeleteNoticeRequest.Marshal(b, m, deterministic)
 }
-func (m *DeleteNoticeRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_DeleteNoticeRequest.Merge(m, src)
+func (dst *DeleteNoticeRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_DeleteNoticeRequest.Merge(dst, src)
 }
 func (m *DeleteNoticeRequest) XXX_Size() int {
 	return xxx_messageInfo_DeleteNoticeRequest.Size(m)
@@ -192,17 +188,16 @@ func (m *UpdateNoticeRequest) Reset()         { *m = UpdateNoticeRequest{} }
 func (m *UpdateNoticeRequest) String() string { return proto.CompactTextString(m) }
 func (*UpdateNoticeRequest) ProtoMessage()    {}
 func (*UpdateNoticeRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4805187b6be1089, []int{3}
+	return fileDescriptor_notice_49e39b4df5242b86, []int{3}
 }
-
 func (m *UpdateNoticeRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_UpdateNoticeRequest.Unmarshal(m, b)
 }
 func (m *UpdateNoticeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_UpdateNoticeRequest.Marshal(b, m, deterministic)
 }
-func (m *UpdateNoticeRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_UpdateNoticeRequest.Merge(m, src)
+func (dst *UpdateNoticeRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UpdateNoticeRequest.Merge(dst, src)
 }
 func (m *UpdateNoticeRequest) XXX_Size() int {
 	return xxx_messageInfo_UpdateNoticeRequest.Size(m)
@@ -237,17 +232,16 @@ func (m *GetNoticeListRequest) Reset()         { *m = GetNoticeListRequest{} }
 func (m *GetNoticeListRequest) String() string { return proto.CompactTextString(m) }
 func (*GetNoticeListRequest) ProtoMessage()    {}
 func (*GetNoticeListRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4805187b6be1089, []int{4}
+	return fileDescriptor_notice_49e39b4df5242b86, []int{4}
 }
-
 func (m *GetNoticeListRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_GetNoticeListRequest.Unmarshal(m, b)
 }
 func (m *GetNoticeListRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_GetNoticeListRequest.Marshal(b, m, deterministic)
 }
-func (m *GetNoticeListRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GetNoticeListRequest.Merge(m, src)
+func (dst *GetNoticeListRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetNoticeListRequest.Merge(dst, src)
 }
 func (m *GetNoticeListRequest) XXX_Size() int {
 	return xxx_messageInfo_GetNoticeListRequest.Size(m)
@@ -269,17 +263,16 @@ func (m *GetNoticeListResponse) Reset()         { *m = GetNoticeListResponse{} }
 func (m *GetNoticeListResponse) String() string { return proto.CompactTextString(m) }
 func (*GetNoticeListResponse) ProtoMessage()    {}
 func (*GetNoticeListResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4805187b6be1089, []int{5}
+	return fileDescriptor_notice_49e39b4df5242b86, []int{5}
 }
-
 func (m *GetNoticeListResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_GetNoticeListResponse.Unmarshal(m, b)
 }
 func (m *GetNoticeListResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_GetNoticeListResponse.Marshal(b, m, deterministic)
 }
-func (m *GetNoticeListResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GetNoticeListResponse.Merge(m, src)
+func (dst *GetNoticeListResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetNoticeListResponse.Merge(dst, src)
 }
 func (m *GetNoticeListResponse) XXX_Size() int {
 	return xxx_messageInfo_GetNoticeListResponse.Size(m)
@@ -298,37 +291,13 @@ func (m *GetNoticeListResponse) GetNotices() []*NoticeData {
 }
 
 func init() {
-	proto.RegisterEnum("NoticeType", NoticeType_name, NoticeType_value)
 	proto.RegisterType((*NoticeData)(nil), "NoticeData")
 	proto.RegisterType((*CreateNoticeRequest)(nil), "CreateNoticeRequest")
 	proto.RegisterType((*DeleteNoticeRequest)(nil), "DeleteNoticeRequest")
 	proto.RegisterType((*UpdateNoticeRequest)(nil), "UpdateNoticeRequest")
 	proto.RegisterType((*GetNoticeListRequest)(nil), "GetNoticeListRequest")
 	proto.RegisterType((*GetNoticeListResponse)(nil), "GetNoticeListResponse")
-}
-
-func init() { proto.RegisterFile("notice/notice.proto", fileDescriptor_a4805187b6be1089) }
-
-var fileDescriptor_a4805187b6be1089 = []byte{
-	// 280 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x92, 0x41, 0x4b, 0xfb, 0x40,
-	0x10, 0xc5, 0xb3, 0xff, 0x7f, 0x6d, 0xed, 0x54, 0xa5, 0x4c, 0xd2, 0x12, 0x7a, 0x31, 0x2e, 0x08,
-	0xc1, 0xc3, 0x0a, 0x2d, 0x78, 0x14, 0xc1, 0x42, 0x2f, 0x22, 0x12, 0xf4, 0x03, 0xc4, 0x76, 0x0e,
-	0x81, 0x90, 0x5d, 0xbb, 0xa3, 0xd0, 0x8f, 0xec, 0xb7, 0x90, 0x64, 0x8d, 0xc4, 0xb2, 0x17, 0xd1,
-	0xd3, 0x32, 0xfb, 0xf6, 0xbd, 0x79, 0xfc, 0x58, 0x08, 0x2b, 0xcd, 0xc5, 0x9a, 0x2e, 0xdd, 0xa1,
-	0xcc, 0x56, 0xb3, 0x96, 0x2b, 0x80, 0xfb, 0x66, 0x5e, 0xe6, 0x9c, 0xe3, 0x29, 0xf4, 0x78, 0x67,
-	0x28, 0x16, 0x89, 0x48, 0x4f, 0xe6, 0x23, 0xe5, 0xa4, 0xc7, 0x9d, 0xa1, 0xac, 0x11, 0x30, 0x86,
-	0xc1, 0x5a, 0x57, 0x4c, 0x15, 0xc7, 0xff, 0x12, 0x91, 0x0e, 0xb3, 0x76, 0x94, 0x0f, 0x10, 0xde,
-	0x6e, 0x29, 0x67, 0x72, 0x9e, 0x8c, 0x5e, 0x5e, 0xc9, 0xf2, 0x6f, 0x12, 0xaf, 0x20, 0x5c, 0x52,
-	0x49, 0x3f, 0x4d, 0xac, 0x9b, 0x3c, 0x99, 0xcd, 0x5f, 0x36, 0x99, 0x42, 0xb4, 0x22, 0x76, 0x86,
-	0xbb, 0xc2, 0xf2, 0x67, 0xa4, 0xbc, 0x86, 0xc9, 0xde, 0xbd, 0x35, 0xba, 0xb2, 0x84, 0xe7, 0x30,
-	0x70, 0x94, 0x6d, 0x2c, 0x92, 0xff, 0xe9, 0xe8, 0x6b, 0x5d, 0x4d, 0x39, 0x6b, 0xb5, 0x8b, 0xb3,
-	0x16, 0x7e, 0xdd, 0x02, 0x0f, 0xa1, 0x57, 0xbe, 0x59, 0x1a, 0x07, 0x38, 0x84, 0x83, 0x4d, 0x5e,
-	0x94, 0xc5, 0x58, 0xcc, 0xdf, 0x05, 0xf4, 0xdd, 0x1b, 0x5c, 0xc0, 0x51, 0x97, 0x30, 0x46, 0xca,
-	0x03, 0x7c, 0xd6, 0xdd, 0x24, 0x83, 0xda, 0xd4, 0x85, 0x88, 0x91, 0xf2, 0x30, 0xf5, 0x98, 0xba,
-	0x04, 0x31, 0x52, 0x1e, 0xa0, 0xfb, 0xa6, 0x1b, 0x38, 0xfe, 0x06, 0x03, 0x27, 0xca, 0x07, 0x6d,
-	0x36, 0x55, 0x5e, 0x66, 0x32, 0x78, 0xee, 0x37, 0x5f, 0x72, 0xf1, 0x11, 0x00, 0x00, 0xff, 0xff,
-	0x5a, 0x6f, 0x56, 0xbc, 0xa9, 0x02, 0x00, 0x00,
+	proto.RegisterEnum("NoticeType", NoticeType_name, NoticeType_value)
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -500,4 +469,28 @@ var _Notice_serviceDesc = grpc.ServiceDesc{
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "notice/notice.proto",
+}
+
+func init() { proto.RegisterFile("notice/notice.proto", fileDescriptor_notice_49e39b4df5242b86) }
+
+var fileDescriptor_notice_49e39b4df5242b86 = []byte{
+	// 280 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x92, 0x41, 0x4b, 0xfb, 0x40,
+	0x10, 0xc5, 0xb3, 0xff, 0x7f, 0x6d, 0xed, 0x54, 0xa5, 0x4c, 0xd2, 0x12, 0x7a, 0x31, 0x2e, 0x08,
+	0xc1, 0xc3, 0x0a, 0x2d, 0x78, 0x14, 0xc1, 0x42, 0x2f, 0x22, 0x12, 0xf4, 0x03, 0xc4, 0x76, 0x0e,
+	0x81, 0x90, 0x5d, 0xbb, 0xa3, 0xd0, 0x8f, 0xec, 0xb7, 0x90, 0x64, 0x8d, 0xc4, 0xb2, 0x17, 0xd1,
+	0xd3, 0x32, 0xfb, 0xf6, 0xbd, 0x79, 0xfc, 0x58, 0x08, 0x2b, 0xcd, 0xc5, 0x9a, 0x2e, 0xdd, 0xa1,
+	0xcc, 0x56, 0xb3, 0x96, 0x2b, 0x80, 0xfb, 0x66, 0x5e, 0xe6, 0x9c, 0xe3, 0x29, 0xf4, 0x78, 0x67,
+	0x28, 0x16, 0x89, 0x48, 0x4f, 0xe6, 0x23, 0xe5, 0xa4, 0xc7, 0x9d, 0xa1, 0xac, 0x11, 0x30, 0x86,
+	0xc1, 0x5a, 0x57, 0x4c, 0x15, 0xc7, 0xff, 0x12, 0x91, 0x0e, 0xb3, 0x76, 0x94, 0x0f, 0x10, 0xde,
+	0x6e, 0x29, 0x67, 0x72, 0x9e, 0x8c, 0x5e, 0x5e, 0xc9, 0xf2, 0x6f, 0x12, 0xaf, 0x20, 0x5c, 0x52,
+	0x49, 0x3f, 0x4d, 0xac, 0x9b, 0x3c, 0x99, 0xcd, 0x5f, 0x36, 0x99, 0x42, 0xb4, 0x22, 0x76, 0x86,
+	0xbb, 0xc2, 0xf2, 0x67, 0xa4, 0xbc, 0x86, 0xc9, 0xde, 0xbd, 0x35, 0xba, 0xb2, 0x84, 0xe7, 0x30,
+	0x70, 0x94, 0x6d, 0x2c, 0x92, 0xff, 0xe9, 0xe8, 0x6b, 0x5d, 0x4d, 0x39, 0x6b, 0xb5, 0x8b, 0xb3,
+	0x16, 0x7e, 0xdd, 0x02, 0x0f, 0xa1, 0x57, 0xbe, 0x59, 0x1a, 0x07, 0x38, 0x84, 0x83, 0x4d, 0x5e,
+	0x94, 0xc5, 0x58, 0xcc, 0xdf, 0x05, 0xf4, 0xdd, 0x1b, 0x5c, 0xc0, 0x51, 0x97, 0x30, 0x46, 0xca,
+	0x03, 0x7c, 0xd6, 0xdd, 0x24, 0x83, 0xda, 0xd4, 0x85, 0x88, 0x91, 0xf2, 0x30, 0xf5, 0x98, 0xba,
+	0x04, 0x31, 0x52, 0x1e, 0xa0, 0xfb, 0xa6, 0x1b, 0x38, 0xfe, 0x06, 0x03, 0x27, 0xca, 0x07, 0x6d,
+	0x36, 0x55, 0x5e, 0x66, 0x32, 0x78, 0xee, 0x37, 0x5f, 0x72, 0xf1, 0x11, 0x00, 0x00, 0xff, 0xff,
+	0x5a, 0x6f, 0x56, 0xbc, 0xa9, 0x02, 0x00, 0x00,
 }
